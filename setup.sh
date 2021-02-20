8b66d48e0989999c95ce107628123dd382f21fa8
Kevin Ruscoe <kevdotbadger@gmail.com>
2021-02-20 11:21:30 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 # update
 apt-get update
 
-# install tools we (may) need
+# install tools we need
 echo "\033[0;32m"
-echo "> Installing tools"
+echo "> Installing needed linux tools"
 echo "\033[0m"
 
 apt-get -y install zip unzip apt-transport-https
@@ -17,7 +17,7 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update
 
-# install software
+# install apache, php74, mysql and elasticsearch
 echo "\033[0;32m"
 echo "> Installing apache, php74, mysql and elasticsearch"
 echo "\033[0m"
@@ -95,12 +95,53 @@ echo "\033[0m"
 
 ufw allow "Apache Full"
 
+# install magento
 echo "\033[0;32m"
-echo "> Hello!"
-echo "> You now have to make a database, please go to https://devdocs.magento.com/guides/v2.4/install-gde/prereq/mysql.html#instgde-prereq-mysql-config"
-echo "> After that, go grab your magento keys from https://marketplace.magento.com/customer/accessKeys/"
-echo "> Then, install magento following:"
-echo "1. https://devdocs.magento.com/guides/v2.4/install-gde/composer.html#get-the-metapackage"
-echo "2. https://devdocs.magento.com/guides/v2.4/install-gde/composer.html#set-file-permissions"
-echo "3. https://devdocs.magento.com/guides/v2.4/install-gde/composer.html#install-magento"
+echo "> Hello! There are a couple of things YOU need to do"
+echo "> 1. Create a database"
+echo "> READ: https://devdocs.magento.com/guides/v2.4/install-gde/prereq/mysql.html#instgde-prereq-mysql-config"
+echo "> run"
+echo "mysql -u root -p"
+echo "create database magento;"
+echo "create user 'magento'@'localhost' IDENTIFIED BY 'magento';"
+echo "GRANT ALL ON magento.* TO 'magento'@'localhost';"
+echo "flush privileges;"
+echo ""
+
+echo "> 2. Install Mangeto via composer"
+echo "> READ: https://devdocs.magento.com/guides/v2.4/install-gde/composer.html#get-the-metapackage"
+echo "> You'll need to go to https://marketplace.magento.com/customer/accessKeys/ to get your access keys"
+echo "> run"
+echo "composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition /var/www/html"
+echo ""
+
+echo "> 3. Clean up permissions"
+echo "> READ: https://devdocs.magento.com/guides/v2.4/install-gde/composer.html#set-file-permissions"
+echo "> run"
+echo "cd /var/www/html/"
+echo "find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +"
+echo "find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +"
+echo "chown -R www-data:www-data . # Ubuntu"
+echo "chmod u+x bin/magento"
+echo ""
+
+echo "> 4. Setup Magento"
+echo "3. READ: https://devdocs.magento.com/guides/v2.4/install-gde/composer.html#install-magento"
+echo "> run"
+echo 'bin/magento setup:install \'
+echo '--base-url=http://YOUR_IP_ADDRESS/ \'
+echo '--db-host=localhost \'
+echo '--db-name=magento \'
+echo '--db-user=magento \'
+echo '--db-password=magento \'
+echo '--admin-firstname=admin \'
+echo '--admin-lastname=admin \'
+echo '--admin-email=admin@admin.com \'
+echo '--admin-user=admin \'
+echo '--admin-password=admin123 \'
+echo '--language=en_GB \'
+echo '--currency=GBP \'
+echo '--timezone=Europe/London \'
+echo '--use-rewrites=1'
+
 echo "\033[0m"
